fb9a7cab5a8ec680be5dd2e4bcf6d1ae2814152b
Anton Maminov <anton.linux@gmail.com>
2019-06-29 12:57:37 +0300

    raise exception with unknown extensions on initialize

--- spec/common_marker_spec.cr
+++ spec/common_marker_spec.cr
@@ -5,6 +5,16 @@ describe CommonMarker do
     (CommonMarker::CMARK_VERSION).should_not be_nil
   end
 
+  describe "#initialize" do
+    it "raise exception with unknown extensions" do
+      expect_raises Exception, "Unknown extension pipeline" do
+        text = "_Hello_ **world**"
+        extensions = ["pipeline"]
+        md = CommonMarker.new(text, extensions: extensions)
+      end
+    end
+  end
+
   describe "#to_html" do
     it "should parse markdown" do
       text = "_Hello_ **world**"
