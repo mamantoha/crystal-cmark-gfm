6a84bf94353d0a2ca8cef88dcc0016826e406963
Anton Maminov <anton.linux@gmail.com>
2021-12-04 15:56:18 +0200

    arch linux test build

--- src/lib_cmark.cr
+++ src/lib_cmark.cr
@@ -1,4 +1,7 @@
 @[Link(ldflags: "#{__DIR__}/../ext/*.a")]
+
+
+@[Link(ldflags: "-L#{__DIR__}/../ext -lcmark-gfm -lcmark-gfm-extensions")]
 lib LibCmark
   type CmarkParser = Void*
   fun cmark_parser_new(options : LibC::Int) : CmarkParser
